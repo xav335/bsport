b93a99c4472806150b2a0b6323abb8f31ff16cd1
Xavier Gonzalez <xavier@gonzalez.pm>
2015-03-03 16:42:30 +0100

    404.php

---
+++ 404.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<title>Bsport.fr | Salle de sport | Mentions légales</title>
+	<meta name="Description" content="Mentions légales du site internet">
+	<meta name="Keywords" content="mentions légales">
+<?php include('inc/meta.php'); ?>
+	
+</head>
+<body
+	class="<?php if (basename(__FILE__) != "index.php") {echo "page";} ?>">
+
+	<!-- Header -->
+<?php include('inc/header.php'); ?>
+<!-- /Header -->
+
+	<!-- Content -->
+	<div class="row content">
+
+		<div class="large-12 medium-12 small-12 columns">
+			<h1>Cette page est intraouvable sur le site !</h1>
+			<h2>B'SPORT</h2>
+			
+		</div>
+
+	</div>
+	<!-- /Content -->
+
+	<!-- Footer -->
+<?php include('inc/footer.php'); ?>
+<!-- /Footer -->
+
+	<!-- Scripts -->
+<?php include('inc/script.php'); ?>
+
+
+</body>
+</html>
