2165162290e5fb3280bb1327dda8feee6ffc5828
J. Gonzalez <fjavi.gonzalez@gmail.com>
2020-08-12 12:11:45 +0200

    Planning + DB

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if (empty($result)) {
 	
 	<div class="large-4 medium-4 small-12 columns">
 	   <div class="large-12 medium-12 small-12 columns">	
-			<div><a class="suite3" target="_blank" href="https://cloud.heitzsystem.com/heitzfit/1330/app">RESERVATION AQUABIKE</a></div>
+			<div><a class="suite3" target="_blank" href="https://cloud.heitzsystem.com/heitzfit/1330/app/index.html#/app/login">RESERVATION AQUABIKE</a></div>
 		</div>
 		&nbsp;
 		<h1>Actualités du Club</h1>
