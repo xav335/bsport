87dd02d71d528c721e2b7de94eb34685f1dc1192
root <root@vmprod.iconeo.fr>
2021-07-13 19:32:44 +0200

    stats no display https

--- admin/home.php
+++ admin/home.php
@@ -39,7 +39,7 @@ if (empty($result)) {
 	<div class="row">
 		<div class="col-md-8">
 			<h3>Statistiques</h3>
-			<iframe id="laframe" src="http://www.iconeo.fr/awstats/awstats.pl?config=www.sport-one.fr&framename=mainright" style="width:720px;height:600px;" frameborder="1" ></iframe>
+			<iframe id="laframe" src="https://www.sport-one.fr/awstats/awstats.pl?config=www.sport-one.fr&framename=mainright" style="width:720px;height:600px;" frameborder="1" ></iframe>
 		</div>
 		<div class="col-md-4">
 			<div class="panel panel-default">
